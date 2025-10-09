4b9e2497bd29f44f54b5b0d88b2ca95fb3205931
creepereye <iostream1204@gmail.com>
2025-10-09 05:25:52 +0000

    add mongodb

--- init-mongo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mongo <<EOF
-use ${MONGO_INITDB_DATABASE}
-db.createUser({
-  user: "${MONGO_USER}",
-  pwd: "${MONGO_PASSWORD}",
-  roles: [{ role: "readWrite", db: "${MONGO_INITDB_DATABASE}" }]
-})
-EOF
